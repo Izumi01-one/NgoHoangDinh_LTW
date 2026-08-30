00f81aad5f596d46c9eca3301d4184bd033164e5
ngodinh <toukaizumi01@gmail.com>
2026-08-30 15:23:09 +0700

    Delete Week_3_Shopping_WebMCV directory
    Delete before reload new version

--- Week_3_Shopping_WebMCV/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Week_3_Shopping_WebMCV.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
