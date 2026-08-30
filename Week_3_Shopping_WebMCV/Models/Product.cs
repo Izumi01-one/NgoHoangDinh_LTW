2c4527c4d8b8e4b737a02ac5a41e1c409afebfce
ngodinh <toukaizumi01@gmail.com>
2026-08-30 15:41:09 +0700

    Week 3 Assignment
    Week 3 Assignment

---
+++ Week_3_Shopping_WebMCV/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace Week_3_Shopping_WebMCV.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image {  get; set; }
+        public int Price {  get; set; }
+        public int SalePrice {  get; set; }
+        public int CategoryId {  get; set; }
+        public string Decription {  get; set; }
+        public int Status {  get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
